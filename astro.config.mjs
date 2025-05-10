48d5b336e43cca2a55e6b0219926e16868bcbc87
Cora Albers <cralbers3@gmail.com>
2025-05-09 22:25:07 -0400

    added in astro-icon package

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,35 +2,35 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Smith Lab',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/cralbers/smithlab-planet' }],
-			sidebar: [
-				{
-					label: 'Hi',
-					autogenerate: { directory: 'hi' },
-					// items: [
-						// Each item here is one entry in the navigation menu.
-						// { label: 'Example Guide', slug: 'guides/example' },
-					// ]
-					
-				},
-				// @ts-ignore
-				{
-					label: 'Protocols',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    integrations: [starlight({
+        title: 'smith lab',
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/cralbers/smithlab-planet' }],
+        sidebar: [
+            {
+                label: 'start here',
+                autogenerate: { directory: 'start' },
+                // items: [
+                    // Each item here is one entry in the navigation menu.
+                    // { label: 'Example Guide', slug: 'guides/example' },
+                // ]
+                
+            },
+            // @ts-ignore
+            {
+                label: 'Protocols',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Example Guide', slug: 'guides/example' },
+                ],
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+        ],
+		}), icon()],
 });
